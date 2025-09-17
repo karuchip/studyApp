b3e05d2f983e031d470d46da4f25777e86129e31
Hikaru Hatakeyama <hkr.12karu@gmail.com>
2025-09-17 17:56:58 +0900

    [modified] home画面を編集

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,11 @@
-import { supabase } from "../lib/supabase"
+import Link from "next/link";
+
 
 export default async function Home() {
-  const { data: records, error } = await supabase
-    .from("record")
-    .select("*")
-    // .order("created_at", { ascending: false });
 
-  if (error) {
-    console.error(error)
-    return <div>エラーが発生しました</div>
-  }
-
-  if (records) {
-    return (
-      <main className="p-4">
-        <h1 className="text-xl font-bold mb-4">勉強記録</h1>
-        <ul>
-          {records?.map((r) => (
-            <li key={r.id}>
-              {r.material_id} - {r.time}分 - {r.created_at}
-              {r.comment}
-            </li>
-          ))}
-        </ul>
-      </main>
-    )
-
-  }
+  return(
+    <>
+      <Link href="/user/login">ログイン画面へ</Link>
+    </>
+  )
 }
